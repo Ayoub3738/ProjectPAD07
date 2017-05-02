f8ae2edc5c7294f9f06fec1f7df69b27dbd562a0
Jerraloid <jerryblaauw@hotmail.com>
2017-05-02 14:46:08 +0200

    Statusscherm backend aangemaakt

--- SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/ProgressActivity.java
+++ SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/ProgressActivity.java
@@ -2,12 +2,38 @@ package com.example.ayoubelyaghmouri.smokes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProgressActivity extends AppCompatActivity {
+
+    TextView txtBespaard;
+    TextView txtAantalNietGerookteSigaretten;
+    TextView txtStreak;
+
+    DatabaseHelper myDb;
+    Status status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+
+        txtBespaard = (TextView)findViewById(R.id.textView2);
+        txtAantalNietGerookteSigaretten = (TextView)findViewById(R.id.txtAantalNietGerookt);
+        txtStreak = (TextView)findViewById(R.id.txtStreak);
+
+
+        myDb = new DatabaseHelper(this);
+        status = myDb.getUser();
+        showData();
+    }
+
+    public void showData() {
+        if(status == null)
+            return;
+
+        txtBespaard.setText(String.format("€ %.2f", status.berekenGeldBesparingen()));
+        txtAantalNietGerookteSigaretten.setText(status.getNietGerookteSigaretten() + "");
+        txtStreak.setText(status.getStreak() + " Sigaretten");
     }
 }
