b36d393a9c5f5375387bef78d8d11ad0004c9ddc
Ginger Ultee <ginger.ultee@hva.nl>
2017-05-11 14:31:00 +0200

    Merge branch 'master' into besparingenOverzichtBranch
    # Conflicts:
    #
    SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/DatabaseHel
    per.java
    #	SmokeS/app/src/main/res/layout/activity_settings.xml

--- SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/SettingsActivity.java
+++ SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/SettingsActivity.java
@@ -8,12 +8,14 @@ import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
     private Button btnSelectTime;
+    private Button btnAlgemeen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         onBtnSlectTimeClick();
+       onBtnSAlgemeenClick();
     }
 
     public void onBtnSlectTimeClick() {
@@ -23,6 +25,18 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent intent = new Intent(SettingsActivity.this, SelectTimeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onBtnSAlgemeenClick() {
+        btnAlgemeen = (Button) findViewById(R.id.btnAlgemeen);
+        btnAlgemeen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SettingsActivity.this, BesparingenOverzichtActivity.class);
                 startActivity(intent);
             }
         });
