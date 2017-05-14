a18f2c93b3cd35c66b9d67006b0f69baee42fba1
Ayoub3738 <ayoub163738@gmail.com>
2017-05-14 12:47:50 +0200

    Achievements
    Aangemaakt en toegevoegd in ht scherm en de app

--- SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/AchievementActivity.java
+++ SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/AchievementActivity.java
@@ -2,12 +2,18 @@ package com.example.ayoubelyaghmouri.smokes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AchievementActivity extends AppCompatActivity {
+
+    private ImageButton btnBespaardGeld_20;
+    private Status status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement);
+
     }
 }
