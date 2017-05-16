abbbcb371a00496b052f052d42d28b95e9015235
Jerraloid <jerryblaauw@hotmail.com>
2017-05-16 13:42:13 +0200

    sigarettenpak class

--- SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/Sigarettenpak.java
+++ SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/Sigarettenpak.java
@@ -1,5 +1,7 @@
 package com.example.ayoubelyaghmouri.smokes;
 
+import android.provider.ContactsContract;
+
 /**
  * Created by jerry on 2-5-2017.
  */
@@ -10,11 +12,35 @@ public class Sigarettenpak {
     private String merk;
     private int aantalSigaretten;
 
+    public Sigarettenpak(double prijs, String merk, int aantalSigaretten) {
+        this.pakID = 1;
+        this.prijs = prijs;
+        this.merk = merk;
+        this.aantalSigaretten = aantalSigaretten;
+    }
+
     public Sigarettenpak(int pakID, double prijs, String merk, int aantalSigaretten) {
         this.pakID = pakID;
         this.prijs = prijs;
         this.merk = merk;
         this.aantalSigaretten = aantalSigaretten;
+    }
+
+    public static Sigarettenpak getPak(DatabaseHelper db) {
+        Sigarettenpak pak = new Sigarettenpak(8.5, "Malborro", 21);
+        //haalt pak uit database
+
+        return pak;
+    }
+
+    public void insert(DatabaseHelper db) {
+        //insert dit object in db
+    }
+
+    public double berekenPrijsSigaret() {
+
+        //gekke berekening
+        return 5;
     }
 
     public int getPakID() {
