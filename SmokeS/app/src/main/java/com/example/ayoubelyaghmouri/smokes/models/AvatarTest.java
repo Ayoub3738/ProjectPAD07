99ed4dbde8a2a6f9e4940d90e1ad6ba3c4ac1649
Athedron <t_haagsma1998@hotmail.com>
2017-05-20 15:26:45 +0200

    extra ding op vorige commit

--- SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/models/AvatarTest.java
+++ SmokeS/app/src/main/java/com/example/ayoubelyaghmouri/smokes/models/AvatarTest.java
@@ -33,10 +33,9 @@ public class AvatarTest extends AppCompatActivity {
     private Integer[] imagesEyes = {R.drawable.womancartooncharacterfullbrownblue,R.drawable.womancartooncharacterfullbrownbrown,R.drawable.womancartooncharacterfullbrowngreen,
                                     R.drawable.womancartooncharacterfullblondblue, R.drawable.womancartooncharacterfullblondbrown, R.drawable.womancartooncharacterfullblondgreen};
     private Integer[] images = imagesHair;
-    private int spriteSarah  = imagesHair[1];
-
-
     private int j = 1;
+    public int spriteSarah  = getImages(getJ());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +67,7 @@ public class AvatarTest extends AppCompatActivity {
             }
         });
 
+        spriteSarah  = getImages(getJ());
         btnOpslaan.setVisibility(View.INVISIBLE);
         imageSwitcherAvatar.setImageResource(spriteSarah);
     }
